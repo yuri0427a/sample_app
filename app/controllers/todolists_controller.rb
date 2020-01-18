66415132390f81ef24470e964676429406ecc2af
yuri0427a <yuri0427ita@gmail.com>
2020-01-18 16:50:00 +0900

    [Finish]3-5,[Start]3-6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,4 +1,26 @@
 class TodolistsController < ApplicationController
   def new
+  	# Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
+        @list = List.new
   end
+    def create
+        # ストロングパラメーターを使用
+         list = List.new(list_params)
+        # DBへ保存する
+         list.save
+        # トップ画面へリダイレクト
+        redirect_to '/top'
+    end
+   def index 
+      end
+
+    def index
+          @lists = List.all
+    end
+
+    private
+   
+    def list_params
+        params.require(:list).permit(:title, :body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   get 'todolists/new'
 	get 'top' => 'homes#top'
-
 	
+	post 'todolists' => 'todolists#create'
+
+	get 'todolists' => 'todolists#index'
 end
