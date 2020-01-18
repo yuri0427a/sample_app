778ffe56f1b442d27b654d2b5a18ba54359a4a1c
yuri0427a <yuri0427ita@gmail.com>
2020-01-18 18:25:59 +0900

    [Start]3-7

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
 	get 'todolists' => 'todolists#index'
 
 	get 'todolists/:id' => 'todolists#show', as: 'todolist'
+
+	get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
 end
